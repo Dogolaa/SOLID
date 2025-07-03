5840375c00685dadd1ac042fccf62583d55acaa9
dogolaa_ <lucasdogo01@gmail.com>
2025-07-02 21:07:07 -0300

    SRP - Exercicios Sutis e CheckoutService

---
+++ SRP/2-Exemplos-Sutis/1-UserRegistration/maneiraErrada.ts
@@ -0,0 +1,44 @@
+// Está errado porque o DTO está sendo usado tanto para criar o usuário quanto para buscar o usuário,
+// o DTO tem razões diferentes para mudar, assim quebrando o SRP
+
+interface UserDTO {
+  id?: string;
+  name: string;
+  email: string;
+  password?: string;
+  createdAt?: Date;
+}
+
+// Serviço de cadastro de usuário
+class UserRegistrationService {
+  registerUser(user: UserDTO) {
+    if (!user.password) {
+      throw new Error("Senha é obrigatória");
+    }
+    console.log("Usuário registrado:", user);
+  }
+}
+
+// Serviço de consulta de usuário
+class UserQueryService {
+  getUserById(id: string): UserDTO {
+    return {
+      id,
+      name: "Alice",
+      email: "alice@email.com",
+      createdAt: new Date(),
+    };
+  }
+}
+
+// Uso
+const registrationService = new UserRegistrationService();
+registrationService.registerUser({
+  name: "Alice",
+  email: "alice@email.com",
+  password: "123456",
+});
+
+const queryService = new UserQueryService();
+const user = queryService.getUserById("1");
+console.log("Usuário consultado:", user);
---
+++ SRP/2-Exemplos-Sutis/2-FileProcessor/maneiraErrada.ts
@@ -0,0 +1,12 @@
+// Aqui podemos modularizar, porque o formato dos arquivos podem mudar
+
+class FileProcessor {
+  processFile(file: string) {
+    console.log(`Lendo arquivo CSV: ${file}`);
+    const data = [{ name: "Alice", value: 100 }]; // Dados simulados
+
+    // Exportar JSON
+    const jsonReport = JSON.stringify(data);
+    console.log(`Exportando relatório em JSON: ${jsonReport}`);
+  }
+}
